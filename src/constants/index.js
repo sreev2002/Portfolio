c27908553d0b945b1f96b9882d8df4523e9f72e2
P Sreevatsan <sreevatsanp@gmail.com>
2025-03-21 15:26:01 +0530

    Update index.js

--- src/constants/index.js
+++ src/constants/index.js
@@ -149,7 +149,7 @@ import {
       company_name: "T-Works",
       icon: tworks,
       iconBg: "#383E56",
-      date: "Jan 2022 - Jan 2023",
+      date: "April 2024 - Sept 2024",
       points: [
         "Programmed interactive 3D models using React Three Fiber to enhance user engagement and provide dynamic visual experiences.",
         "Developed a custom AI-powered chatbot to streamline client interactions, resulting in a 25% reduction in turnover time",
@@ -162,7 +162,7 @@ import {
       company_name: "T-Works",
       icon: tworks,
       iconBg: "#E6DEDD",
-      date: "Jan 2023 - Present",
+      date: "Oct 2024 - Present",
       points: [
         "Initiated and led the development of a custom ERP solution tailored to specific business case scenarios, enhancing operational efficiency.",
         "Designed and implemented the solution using the T3 stack, integrating it seamlessly with the existing SaaS platforms.",
@@ -202,28 +202,28 @@ import {
     {
       name: "Eco Fertilization",
       description:
-        "Lorem Ipsum",
+        "Engineered a CNN-based model for potato leaf disease classification, preprocessing a labeled dataset to address imbalance and enhance performance. ",
       tags: [
         {
           name: "react",
           color: "blue-text-gradient",
         },
         {
-          name: "mongodb",
+          name: "Python",
           color: "green-text-gradient",
         },
         {
-          name: "tailwind",
+          name: "Supabase",
           color: "pink-text-gradient",
         },
       ],
       image: carrent,
-      source_code_link: "https://github.com/",
+      source_code_link: "",
     },
     {
-      name: "Project 2",
+      name: "AI Driven Tool For Meeting Management  For  Productivity And Engagement  Using LLM   2",
       description:
-        "Lorem Ipsum",
+        "Designed and deployed a smart assistant capable of providing real-time insights and data retrieval during meetings. Received an Accuracy of over 98% in Information Retrieval and query resolving capabilities.  ",
       tags: [
         {
           name: "react",
